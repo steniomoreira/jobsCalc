bc333eeac37921800409ec0e0604f1274c1e0d3a
Stenio Moreira <steniomoreira01@gmail.com>
2021-07-25 18:39:44 -0300

    Create routes.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes");
 
-server.get('/', (request, response) => {
-    return response.send('Nalu!');
-})
+//files statics
+server.use(express.static("public"));
+
+//routes
+server.use(routes);
 
 server.listen(3000, () => console.log('rodando'));
