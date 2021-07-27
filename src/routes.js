10a4fba5351c8905586f3f0e18843a6f95b99103
Stenio Moreira <steniomoreira01@gmail.com>
2021-07-26 22:57:10 -0300

    create object profile

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router();
 
 const views = __dirname + '/views/';
 
+const profile = {
+    name: "Stenio",
+    avatar: "https://github.com/steniomoreira.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
 routes.get('/', (req, res) => res.render(views + "index"));
 routes.get('/job', (req, res) => res.render(views + "job"));
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
-routes.get('/profile', (req, res) => res.render(views + "profile"));
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
 module.exports = routes;
